4894b0db3996f18f6240c34befaf2e46789ac859
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2026-02-16 22:15:02 -0300

    feat: implementar consulta diaria y protección contra duplicados
    - Se añade el endpoint '/daily' en el router para búsquedas por fecha.
    - Se actualiza 'import_training' con lógica de verificación (evita duplicados).
    - Se implementa rango de tiempo (, ) en 'get_training_of_the_day' para mayor precisión.
    - Se actualiza la colección a 'trainings_test_5' y se mejora el manejo de rutas GPX nulas.

--- app/router/trainings.py
+++ app/router/trainings.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, HTTPException, status
 from app.models.trainings import Workout
-from app.services.trainings import import_training, create_training
+from app.services.trainings import (
+    import_training,
+    create_training,
+    get_training_of_the_day,
+)
+from datetime import datetime
 
 router = APIRouter(prefix="/training", tags=["training"])
 
@@ -17,6 +22,15 @@ async def create_training_router(payload: Workout):
 @router.post("/by_path", response_model=Workout, status_code=status.HTTP_201_CREATED)
 async def import_training_router(training_path: str):
     result = await import_training(training_path)
+
+    if not result:
+        raise HTTPException(500, "No se concreta la accion")
+    return result
+
+
+@router.get("/daily", response_model=Workout)
+async def get_training_of_the_day_router(date: datetime) -> dict:
+    result = await get_training_of_the_day(date)
 
     if not result:
         raise HTTPException(500, "No se concreta la accion")
--- app/services/trainings.py
+++ app/services/trainings.py
@@ -1,8 +1,9 @@
 from app.models.trainings import Workout
 from app.database import MongoDBConnectionManager
 from app.services.utils import load_json_from_path, to_out, get_gpx_path
+from datetime import datetime
 
-local_COLLECTION = "trainings_test_4"
+local_COLLECTION = "trainings_test_5"
 
 
 async def create_training(payload: Workout):
@@ -31,9 +32,33 @@ async def import_training(training_path: str):
     gpx_path = get_gpx_path(training_path)
     if gpx_path != "olaNuro":
         doc["gpx_path"] = gpx_path
+    else:
+        doc["gpx_path"] = None
     async with MongoDBConnectionManager() as db:
-        result = await db[local_COLLECTION].insert_one(doc)
-        created = await db[local_COLLECTION].find_one({"_id": result.inserted_id})
-        if not created:
-            return
-    return to_out(created)
+        existe = await db[local_COLLECTION].find_one(
+            {"training_date": doc["training_date"]}
+        )
+        if not existe:
+            result = await db[local_COLLECTION].insert_one(doc)
+            created = await db[local_COLLECTION].find_one({"_id": result.inserted_id})
+            if not created:
+                return {"result": None}
+
+            return to_out(created)
+        else:
+            return to_out(existe)
+
+
+# Funcion GET
+async def get_training_of_the_day(date: datetime) -> dict:
+    dia_inicio = date.replace(hour=0, minute=0, second=0, microsecond=0)
+    dia_fin = date.replace(hour=23, minute=59, second=59)
+
+    async with MongoDBConnectionManager() as db:
+        result = await db[local_COLLECTION].find_one(
+            {"training_date": {"$gt": dia_inicio, "$lt": dia_fin}}
+        )
+
+    if not result:
+        return {"result": None}
+    return result
